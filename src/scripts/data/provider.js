8ae34c262e7a6f2a2d8d232432903f185c187133
Bryce Borba <borbaesb@gmail.com>
2022-01-06 07:13:03 -0800

    set up render for easy addition of future promises

--- src/scripts/data/provider.js
+++ src/scripts/data/provider.js
@@ -2,22 +2,31 @@ const apiURL = "http://localhost:8088";
 const applicationElement = document.querySelector(".giffygram");
 
 const applicationState = {
-  currentUser: {},
-  feed: {
-    chosenUser: null,
-    displayFavorites: false,
-    displayMessages: false,
+  currentState: {
+      users: [],
+      posts: [],
+      likes: [],
+      messages: []
   },
+currentUser: {},
+feed: {
+  chosenUser: null,
+  displayFavorites: false,
+  displayMessages: false,
+      displayNavBar: true,
+      displayMessageCreate: false,
+      displayPostCreate: false
+}
 };
 
 export const fetchUsers = () => {
   return fetch(`${apiURL}/users`)
     .then((response) => response.json())
     .then((serviceRequests) => {
-      applicationState.requests = serviceRequests;
+      applicationState.users = serviceRequests;
     });
 };
 
 export const getUsers = () => {
-  return applicationState.users.map((user) => ({ ...user }));
+  return applicationState.users.map((user) => ({ ...user}));
 };
